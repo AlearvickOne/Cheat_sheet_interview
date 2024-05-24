5c79dfdb667768f06e21d944f468d7e1c6e2e879
AlearvickOne <alexandr73.73.73.73@mail.ru>
2024-05-24 16:26:26 +0300

    get quantity checker

--- src/hooks/useGetQuestions.tsx
+++ src/hooks/useGetQuestions.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import ServerCommands from "../_apiLocal/serverComands.class";
 import { IQuestionsJson } from "@/interfaces/IQuestions.interface";
 
 export const useGetQuestions = () => {
   const [questions, setQuestions] = useState<IQuestionsJson>();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     (async () => {
       await ServerCommands.getQuestionsServer().then((el) => {
         setQuestions(el);
